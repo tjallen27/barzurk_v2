54859c20923e9a1badc70b4154665df97294b2ed
tjallen27 <tjallen91@hotmail.co.uk>
2017-11-01 16:37:57 +0000

    Session flash message update on login

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -19,7 +19,7 @@ function sessionsCreate(req, res, next) {
       req.session.isAuthenticated = true;
       req.user = user;
 
-      req.flash('success', `Welcome back, ${user.username}!`);
+      req.flash('success', `Welcome back, ${user.pubName}!`);
       res.redirect(`/users/${user.id}`);
     })
     .catch(next);
